460f9a88c20f8c0e95999ecd3d46235aac0261ab
rishii64 <saptarsismmitra@gmail.com>
2023-11-10 21:05:01 +0530

    second commit

--- json.js
+++ json.js
@@ -1,7 +1,27 @@
-const data = [{
-    name: "Rishi", 
-    age: 23, 
-    course: "MERN", 
-    batch: "EA25"
-}]
+const data = [
+    {
+        name: "Rishi",
+        age: 23,
+        course: "MERN",
+        batch: "EA25"
+    },
+    {
+        name: "Mahi",
+        age: 22,
+        course: "MEAN",
+        batch: "EA25"
+    },
+    {
+        name: "Saptarsi", 
+        age: 23, 
+        course: "Full Stack", 
+        batch: "EA25"
+    },
+    {
+        name: "Anshuman", 
+        age: 22, 
+        course: "Front-End", 
+        batch: "EA25"
+    }
+]
 module.exports = data
